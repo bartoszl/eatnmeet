b95e8901e9f269b72d92c6e897931a3480cd9855
Bartosz Lewandowski <bartosz@Bartoszs-MacBook-Pro.local>
2017-03-27 13:37:01 +0100

    added dropdowns for navbar and filter button

--- js/app.js
+++ js/app.js
@@ -174,6 +174,16 @@ var app = angular.module('myApp', ['ngRoute'])
                     picture: '/images/event6.jpg'
                 }];
 
+            var filter = document.getElementById('filter');
+
+            $scope.toggleFilter = function() {
+              if(!filter.classList.contains("show")) {
+                filter.classList.add("show");
+              } else {
+                filter.classList.remove("show");
+              }
+            };
+
             $scope.eventList = events;
             $scope.people = people;
             $scope.currentUserId = 3;
